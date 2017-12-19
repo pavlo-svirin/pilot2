729e7942242066df514f0fe8868609d22b7bc3eb
PalNilsson <paul.nilsson@cern.ch>
2017-12-19 22:41:25 +0100

    Update

--- pilot/control/payload.py
+++ pilot/control/payload.py
@@ -453,5 +453,5 @@ def failed_post(queues, traces, args):
 
         log.debug('adding jog for log stageout')
 
-        queues.data_out.put(job)
+        queues.data_out.put(failedjob)
 # wrong queue??
