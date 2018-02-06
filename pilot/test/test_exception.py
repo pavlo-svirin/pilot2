dcbec77103053a768d981a81cf6b045a677875f2
PalNilsson <paul.nilsson@cern.ch>
2018-02-06 17:29:54 +0100

    Changed error code 1111 to 1301

--- pilot/test/test_exception.py
+++ pilot/test/test_exception.py
@@ -37,17 +37,13 @@ class TestException(unittest.TestCase):
             raise RunPayloadFailure(a='message a', b='message b')
         except PilotException as ex:
             self.assertIsInstance(ex, PilotException)
-            self.assertEqual(ex.get_error_code(), 1305)
-            logging.info("\nException: error code: %s\n\nMain message: %s\n\nFullStack: %s" % (ex.get_error_code(),
-                                                                                               str(ex),
-                                                                                               ex.get_detail()))
+            self.assertEqual(ex.get_error_code(), 1301)
+            logging.info("\nException: error code: %s\n\nMain message: %s\n\nFullStack: %s" % (ex.get_error_code(), str(ex), ex.get_detail()))
 
         try:
             pass
             raise RunPayloadFailure("Test message")
         except PilotException as ex:
             self.assertIsInstance(ex, PilotException)
-            self.assertEqual(ex.get_error_code(), 1305)
-            logging.info("\nException: error code: %s\n\nMain message: %s\n\nFullStack: %s" % (ex.get_error_code(),
-                                                                                               str(ex),
-                                                                                               ex.get_detail()))
+            self.assertEqual(ex.get_error_code(), 1301)
+            logging.info("\nException: error code: %s\n\nMain message: %s\n\nFullStack: %s" % (ex.get_error_code(), str(ex), ex.get_detail()))
